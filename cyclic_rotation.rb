4f0c053e07e3e3928768fbd8d7158d5209b96c2d
Domita <dominikasiek@gmail.com>
2017-08-19 21:42:28 +0200

    Tabs changed into spaces

--- cyclic_rotation.rb
+++ cyclic_rotation.rb
@@ -1,23 +1,25 @@
 
 def solution(arr, k)
 
-	for i in 1..k
+  for i in 1..k
 
     new_arr = (0..arr.length - 1).to_a
 
-		arr.each_with_index do |x, index|
+    arr.each_with_index do |x, index|
       case index
         when index == 0 then new_arr[0] = arr[arr.length - 1] 
         when index == arr.length - 1 then new_arr[index] = arr[0] 
-   		  else new_arr[index] = arr[index - 1] 
-   		end
+        else new_arr[index] = arr[index - 1] 
+       end
     end
     
     arr = new_arr
-	end
+  end
 
-	return arr
+  return arr
 end
 
 print solution([3, 8, 9, 7, 6], 3)
+
+
 
