8c3e41c8d68c49313e5f2e51abae3556bd849d3d
Dominika <dominikasiek@gmail.com>
2019-09-15 19:22:46 +0200

    Add alternative solutions.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -22,3 +22,51 @@ def bubble_sort(array)
   end
   array
 end
+
+# alternative solutions
+
+# def bubble_sort(array)
+#   status = true
+
+#   while status
+#     status = false
+
+#     array.each_with_index do |element|
+
+#       current_element = array[index]
+#       next_element    = array[index + 1]
+
+#       if current_element > next_element
+#         array[index]     = next_element
+#         array[index + 1] = current_element
+#         status           = true
+#       end
+
+#     end
+#   end
+#   array
+# end
+
+
+# def bubble_sort(array)
+#   status = true
+
+#   while status
+#     status = false
+#     i      = 0
+
+#     for i in (array_length - 1) do
+
+#       current_element = array[i]
+#       next_element    = array[i + 1]
+
+#       if current_element > next_element
+#         array[i]     = next_element
+#         array[i + 1] = current_element
+#         status       = true
+#       end
+
+#     end
+#   end
+#   array
+# end
