1c83c50221b1af71f07192c18f0baabde5175eca
Dominika <dominikasiek@gmail.com>
2019-09-22 10:10:43 +0200

    Add remove duplicate elements.

---
+++ remove_duplicate_elements.rb
@@ -0,0 +1,19 @@
+def remove_duplicate_elements(array)
+
+  elements = {}
+
+  array.each do |x|
+    if elements[x]
+      elements[x] += 1
+    else
+      elements[x] = 1
+    end
+  end
+
+  elements
+  elements.keys
+
+end
+
+# Task description: https://www.geeksforgeeks.org/remove-duplicate-elements-in-an-array-using-stl-in-c/
+
